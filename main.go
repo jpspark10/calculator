2c617d79a004f9ceb2ea89828678dc4159a67121
Vladimir <90626549+jpspark10@users.noreply.github.com>
2024-04-15 08:58:00 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"calculator-main/pkg/handlers"
 	"fmt"
-	"html/template"
 	"net/http"
 )
-
-var templates *template.Template
 
 func main() {
 	http.HandleFunc("/", handlers.MainHandler)
 	http.HandleFunc("/calc", handlers.CalcHandler)
 	http.HandleFunc("/login", handlers.LoginHandler)
-	templates = template.Must(template.ParseGlob("templates/*.html"))
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
